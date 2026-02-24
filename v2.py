dd613bbf4e6b85dff780427fbf719fe895b810b3
burk3405 <acburkett04@gmail.com>
2026-02-24 09:33:03 -0500

    Implement state printing and dirtification functions
    Added functions to print and modify the state of a grid.

---
+++ v2.py
@@ -0,0 +1,26 @@
+import random
+
+def printState(st):
+    print("---------------------")
+    for row in st:
+        for col in row:
+            print(col, end = "\t")
+        print()
+    print("---------------------")
+    
+def dirtifyState(st):
+    for count in range(5):            
+        row = random.randint(0,2)
+        col = random.randint(0,2)
+    
+        st[row][col] = 'Dirty'
+
+start_state = [['Clean','Clean','Clean'],
+               ['Clean','Clean','Clean'],
+               ['Clean','Clean','Clean']]
+               
+printState(start_state)
+  
+dirtifyState(start_state)
+    
+printState(start_state)
